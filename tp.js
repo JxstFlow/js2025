0977aabec4f434d4dd96e778cf1eec92572e8dc7
Florian Iz <theflo30@gmail.com>
2025-11-06 05:17:52 +0100

    deuxième exo tp.js

--- tp.js
+++ tp.js
@@ -1,8 +1,18 @@
+ let a = 12;
+ let b = 45;
+ let c = a + b;
+
+ // Affichage du résultat dans la console
+> console.log("La valeur de c est :", c);
+
+//affichage résultat 
+node.js
+
 // variable Hello et World 
 let bonjour = "Hello";
 let tout_le_monde = "World";
 
-// Concaténation et affichage dans la console
+// Concaténation + affichage dans la console
 console.log(bonjour + " " + tout_le_monde);
 
 // Afficher le résultat 
