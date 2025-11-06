a7f63e9151d7f63aad4ab62b3ed33384ec819ade
Florian Iz <theflo30@gmail.com>
2025-11-06 05:48:38 +0100

    Personne mineur ou majeur

---
+++ exo3.js
@@ -0,0 +1,10 @@
+// Age pris au hasard 
+let age = 26;
+
+// Vérification avec la structure if
+if (age >= 18) {
+    console.log("La personne est majeure.");
+} else {
+    console.log("La personne est mineure.");
+}
+
